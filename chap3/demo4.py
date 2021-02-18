b7136d51224c8b701c54bff562188c2d86ae92b7
Cateax <souzeonpaang@gmail.com>
2021-02-18 20:47:56 +0800

    初次学习Python，希望能坚持下去。

---
+++ chap3/demo4.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/2/18 17:41
+# @Author  : Cateax
+
+# 变量由标识、类型和值组成
+
+a = 10
+b = 10
+print(a == b) # 比较的是value
+print(a is b) # 比较对象的标识
+print(a is not b)
